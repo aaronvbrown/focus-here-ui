5a818155e7f011e9364d92b0b1a3c3b757fdedc1
Aaron Brown <worldofaaron@hotmail.com>
2024-02-25 10:46:58 -0800

    further develop Students

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
 function Students() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:7095/students')
-      .then(response => response.json())
-      .then(data => setStudents(data));
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}: ${response.statusText}`);
+        } else {
+          return response.json()
+        }
+      })
+      .then((jsonifiedResponse) => {
+        setStudents(jsonifiedResponse);
+        setIsLoaded(true);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoaded(true);
+      });
   }, []);
 
-  return (
-    <div>
-      <h1>Students</h1>
-      <ul>
-        {students.map(student => (
-          <li key={student.id}>{student.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  if (error) {
+    return <h1>Error: </h1>
+  } else if (!isLoaded) {
+    return <h1>...Loading...</h1>
+  } else {
+    return (
+      <React.Fragment>
+        <div>
+          <h1>Students</h1>
+          <ul>
+            {students.map(student => (
+              <li key={student.id}>{student.name}</li>
+            ))}
+          </ul>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
+
+export default Students;
